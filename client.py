536bbee9102d971339545031563b0aa643bd564f
jp05451 <59051826+jp05451@users.noreply.github.com>
2024-03-27 22:13:29 +0800

    add client socket connection

--- client.py
+++ client.py
@@ -4,8 +4,11 @@ from Crypto.PublicKey import RSA
 class client():
     def __init__(self):
         self.key = RSA.generate(2048)
+        self.clientSocket=socket()
 
     def getPublicKey(self):
         return self.key.publickey().exportKey("PEM")
 
-    # def 
+    def connectSocket(self,addr,port):
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP宣告
+        self.clientSocket.connect(addr,port)
