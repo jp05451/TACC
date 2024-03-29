7ea3a29d7e4f0318f5a1a62f63f4413edfdebd66
jp05451 <59051826+jp05451@users.noreply.github.com>
2024-03-29 17:23:44 +0800

    finish server: decode encode function

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from socket import socket
-from Crypto.PublicKey import RSA
+from base64 import b64decode,b64encode
+# import random
+
 from Crypto.Cipher import PKCS1_OAEP
-import shamirs
 
 
 class server:
     def __init__(self):
         self.serverSocket = socket()
+        self.share = 0
 
     def socketConnect(self, addr, port):
         self.serverSocket.bind((addr, port))
@@ -14,18 +16,34 @@ class server:
 
     def __del__(self):
         self.serverSocket.close()
-        
+
     def recvPublicKey(self):
-        client,addr=self.serverSocket.accept()
-        self.clientPublicKey=client.recv(1024).decode()
+        client, addr = self.serverSocket.accept()
+        self.clientPublicKey = client.recv(1024).decode()
         client.close()
+
+    def RSA_Decrypt(self, cypherText,key):
+        cipher = PKCS1_OAEP.new(key)
+        plainText = cipher.decrypt(b64decode(cypherText))
+        return plainText.decode("utf-8")
+
+    def RSA_Encrypt(self, msg,key):
+        cipher = PKCS1_OAEP.new(key)
+        cypherText = cipher.encrypt(msg.encode("utf-8"))
+        return b64encode(cypherText).decode()
+
+    def SendKeyShares(self):
+        client, addr = self.serverSocket.accept()
+        clientRequest = client.recv(1024).decode()
         
-    # def 
         
+        if clientRequest == "1234":
+            client.send(self.RSA_Encrypt(self.share))
+
+        client.close()
+
 if __name__ == "__main__":
-    Server=server()
-    Server.socketConnect("0.0.0.0",8888)
+    Server = server()
+    Server.socketConnect("0.0.0.0", 8888)
     Server.recvPublicKey()
     print(Server.clientPublicKey)
-        
-        
