bac2041db2a78bb43e77f6f11514603615085c1e
jp05451 <59051826+jp05451@users.noreply.github.com>
2024-04-11 17:50:00 +0800

    add test bench

--- client.py
+++ client.py
@@ -25,15 +25,14 @@ class client:
     def connectSocket(self, addr, port):
         self.clientSocket.connect((addr, port))
 
-    def RSA_Decrypt(self, cypherText, key):
-        # key = RSA.importKey(self.getPublicKey())
+    def RSA_Decrypt(self, cypherText):
+        key = RSA.importKey(self.getPrivateKey())
         cipher = PKCS1_OAEP.new(key)
         plainText = cipher.decrypt(b64decode(cypherText))
         return plainText
 
-    def RSA_Encrypt(self, msg,key):
-        
-        # key = RSA.importKey(self.getPrivateKey())
+    def RSA_Encrypt(self, msg):
+        key = RSA.importKey(self.getPrivateKey())
         cipher = PKCS1_OAEP.new(key)
         cypherText = cipher.encrypt(msg.encode("utf-8"))
         # return cypherText
@@ -45,15 +44,9 @@ class client:
 
 if __name__ == "__main__":
     Client = client()
-    
-    Client.connectSocket("127.0.0.1",8888)
+
+    Client.connectSocket("127.0.0.1", 8888)
     Client.sendPublicKey()
-    
-    # Client.connectSocket("127.0.0.1",8888)
-    
-    sleep(1000)
-    
-    print("sending 1234")
-    Client.clientSocket.send("1234".encode("utf-8"))
-    # cypher=Client.clientSocket.recv(4096).decode()
-    # text=Client.RSA_Decrypt(cypher,RSA.importKey(Client.getPrivateKey()))
+    encryptKeyShare = Client.clientSocket.recv(1024).decode()
+    keyshare=Client.RSA_Decrypt(encryptKeyShare)
+    print(keyshare)
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from base64 import b64decode,b64encode
 
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
+import shamirs
 
 
 class server:
@@ -16,6 +17,7 @@ class server:
         self.serverSocket.listen(5)
 
     def __del__(self):
+        print("connection close")
         self.serverSocket.close()
 
     
@@ -26,27 +28,47 @@ class server:
         return plainText.decode("utf-8")
 
     def RSA_Encrypt(self, msg,key):
+        # key=RSA.importKey(self.getPublicKey())
         cipher = PKCS1_OAEP.new(key)
         cypherText = cipher.encrypt(msg.encode("utf-8"))
         return b64encode(cypherText).decode()
 
     def recvPublicKey(self):
         client, addr = self.serverSocket.accept()
-        self.clientPublicKey = client.recv(1024).decode()
+        self.clientPublicKey = client.recvfrom(1024).decode()
         client.close()
+        
+    def keySharing(self,secret:int,N,T):
+        self.__secretShares = shamirs.shares(secret, quantity=N,threshold=T)
+        
+    # def keySharing(self, secret: int, N):
+    #     self.__secretShares = shamirs.shares(secret, quantity=N)
 
-    def SendKeyShares(self):
-        print("recv socket")
-        client, addr = self.serverSocket.accept()
-        clientRequest = client.recv(1024).decode()
+    def getShare(self,part:int):
+        return self.__secretShares[part]
         
-        print(clientRequest)
-        
-        client.close()
 
 if __name__ == "__main__":
     Server = server()
     Server.socketConnect("0.0.0.0", 8888)
-    Server.recvPublicKey()
-    print(Server.clientPublicKey)
-    Server.SendKeyShares()
+    Server.serverSocket.listen(5)
+
+    connection, addr = Server.serverSocket.accept()
+
+    print('connected by ' + str(addr))
+    print("recv RSA:")
+    publickey=connection.recv(1024)
+    print(publickey)
+    
+    print("sharing key")
+    Server.keySharing(1234,3,2)
+    keyshare=Server.getShare(2)
+    print(keyshare)
+    
+    print("encrypting keyshare")
+    encryptKeyShare=Server.RSA_Encrypt(str(keyshare),RSA.importKey(publickey))
+
+    print("sending encrypted key share")
+    connection.send(encryptKeyShare.encode())
+    
+    connection.close()
