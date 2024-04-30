9cbf3fee401b5d017befc8b14d7288eb5740460d
jp05451 <59051826+jp05451@users.noreply.github.com>
2024-04-30 17:33:19 +0800

    finish IBE

--- IBE.py
+++ IBE.py
@@ -2,69 +2,82 @@ from hashlib import sha256
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
-# from Crypto.Random import get_random_bytes
 from Crypto.Util import number
-from random import Random
+import random
 import shamirs
 
-Random.seed()
+random.seed()
 
 
 class IBE:
     def __init__(self):
         # self.master_key = get_random_bytes(16)  # 128-bit隨機主密鑰
-        self.p = number.getPrime(2**16 - 1)
+        self.p = number.getPrime(2**4 - 1)
+        self.__generateMasterKeyShares(5, 5)
 
-    def generateMasterKeyShares(self, N=5, T=5):
-        masterKey = Random.randint(0, self.p - 1)
-        self.__k = Random.randint(0, self.p - 1)
+    def __generateMasterKeyShares(self, N=5, T=5):
+        masterKey = random.randint(0, self.p - 1)
+        self.__k = random.randint(0, self.p - 1)
 
         # split master key to key shares
         self.__masterkeyShare = shamirs.shares(
             masterKey, quantity=N, threshold=T, modulus=self.p
         )
 
-    def generateSecretKeyShare(self, masterKeyShare: shamirs.share, id):
-        idNumber = int(sha256(id)) % self.p
+    def generateSecretKeyShares(self, session):
+        idNumber = int(sha256(session.encode()).hexdigest(), 16) % self.p
+        # print(idNumber)
 
-        self.__secretKeyShare = []
-        self.__secretKeyShare.append(masterKeyShare.value + idNumber + self.__k)
+        # make secretShare by masterkeyshare + id + k
+        self.__secretKeyShares = [s for s in self.__masterkeyShare]
+
+        for i in range(len(self.__masterkeyShare)):
+            self.__secretKeyShares[i].value = (
+                self.__masterkeyShare[i].value + idNumber + self.__k
+            )%self.p
 
     def getSecretKeyShare(self, keyNumber):
-        return self.__secretKeyShare[keyNumber]
+        return self.__secretKeyShares[keyNumber]
 
-    def getMasterKey(self, keyNumber):
-        return (self.__masterkeyShare[keyNumber], self.__k)
 
-    def generate_secret_key(self, identity):
+    def secretKeyReconstruct(self, secretKeyShares):
         # 將身份和主密鑰組合在一起
-        combined_key = identity.encode() + self.master_key
-        # 使用 SHA-256 哈希來生成身份密鑰
-        hashed_key = sha256(combined_key).digest()
-        return hashed_key
+        print(secretKeyShares)
+        print(shamirs.interpolate(secretKeyShares,5))
+        return shamirs.interpolate(secretKeyShares)
 
-    def encrypt(self, message, identity):
-        secret_key = self.generate_secret_key(identity)
-        cipher = AES.new(secret_key, AES.MODE_CBC)
+    def encrypt(self, message, key: int):
+        
+        cipher = AES.new(key.to_bytes(16,'little'), AES.MODE_CBC)
         ciphertext = cipher.encrypt(pad(message.encode(), AES.block_size))
         iv = cipher.iv
         return ciphertext, iv
 
-    def decrypt(self, ciphertext, iv, identity):
-        secret_key = self.generate_secret_key(identity)
-        cipher = AES.new(secret_key, AES.MODE_CBC, iv=iv)
+    def decrypt(self, ciphertext, iv, key: int):
+        
+        cipher = AES.new(key.to_bytes(16, "little"), AES.MODE_CBC,iv)
         plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
         return plaintext.decode()
 
 
-# 使用範例
-ibe = IBE()
+if __name__ == "__main__":
+    # 使用範例
+    ibe = IBE()
 
-# 加密消息
-message = "Hello, world!"
-identity = "user@example.com"
-ciphertext, iv = ibe.encrypt(message, identity)
+    shares = []
+    print("generating secret key")
+    ibe.generateSecretKeyShares("aaa")
+    for i in range(5):
+        shares.append(ibe.getSecretKeyShare(i))
+    print(shares)
 
-# 解密消息
-decrypted_message = ibe.decrypt(ciphertext, iv, identity)
-print("Decrypted message:", decrypted_message)
+    key = ibe.secretKeyReconstruct(shares)
+    print(key)
+
+    # 加密消息
+    message = "Hello, world!"
+    ciphertext, iv = ibe.encrypt(message, key)
+
+    # 解密消息
+    decrypted_message = ibe.decrypt(ciphertext, iv, key)
+    print("Decrypted message:", decrypted_message)
